35f809d9f778957d167596159e9285db01422ee8
faith-holden <ha.av.sodak@gmail.com>
2022-01-04 11:35:39 -0600

    created some static pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_title)
+    base_title = "StoreIt"
+    if page_title.empty?
+      base_title
+    else
+      page_title + " | " + base_title
+    end
+  end
 end
